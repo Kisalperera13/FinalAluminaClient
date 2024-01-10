5fc6d26f8bae87c0db20a54c5c4f56198546537e
KisalPerera <79179580+Kisalperera13@users.noreply.github.com>
2024-01-10 14:38:19 +0530

    janfirst

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -8,14 +8,23 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
-  const getPosts = async () => {
+  const initialState = {
+    posts: [],
+    // other state properties...
+  };
+const getPosts = async () => {
+  try {
     const response = await fetch("http://localhost:3001/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
+    console.log("API Response:", data);
     dispatch(setPosts({ posts: data }));
-  };
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+};
 
   const getUserPosts = async () => {
     const response = await fetch(
@@ -39,33 +48,39 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   return (
     <>
-      {posts.map(
-        ({
-          _id,
-          userId,
-          firstName,
-          lastName,
-          description,
-          location,
-          picturePath,
-          userPicturePath,
-          likes,
-          comments,
-        }) => (
-          <PostWidget
-            key={_id}
-            postId={_id}
-            postUserId={userId}
-            name={`${firstName} ${lastName}`}
-            description={description}
-            location={location}
-            picturePath={picturePath}
-            userPicturePath={userPicturePath}
-            likes={likes}
-            comments={comments}
-          />
-        )
-      )}
+{
+  Array.isArray(posts) && posts.length > 0 ? (
+    posts.map(
+      ({
+        _id,
+        userId,
+        firstName,
+        lastName,
+        description,
+        location,
+        picturePath,
+        userPicturePath,
+        likes,
+        comments,
+      }) => (
+        <PostWidget
+          key={_id}
+          postId={_id}
+          postUserId={userId}
+          name={`${firstName} ${lastName}`}
+          description={description}
+          location={location}
+          picturePath={picturePath}
+          userPicturePath={userPicturePath}
+          likes={likes}
+          comments={comments}
+        />
+      )
+    )
+  ) : (
+    <p>No posts available.</p>
+  )
+}
     </>
   );
 };
